27528fb5f6fcaafb4ddf148e0a2599e22b5a3ab2
Naveen Reddy Rella <naveenrella@Naveens-MacBook-Air.local>
2018-04-12 02:27:14 -0700

    6 test cases written

--- public/tests/registration_spec.js
+++ public/tests/registration_spec.js
@@ -15,13 +15,16 @@ browser.driver.controlFlow().execute = function() {
 };
 
 describe("checking registration end to end",function(){
+    afterEach(function(done){
+        console.log('calling after all');
+        browser.refresh();
+        process.nextTick(done);
+    });
+
     it("test registration",function(){
         browser.get("http://localhost:3000");
     });
-    it('Page should have a title', function() {
-        expect(browser.getTitle()).toEqual('Parient Registration');
-    });
-
+    
     var signup = new SignUp();
 
     it('1.Happy path,  testing with all given fields \n', function () {
@@ -36,25 +39,48 @@ describe("checking registration end to end",function(){
      });
      it('2. Testing FullName with numbers, \n', function () {
         var inputUserName = element(by.id('fullNameError'));
-
+        
         signup.fillFormFullName("sai345");
         signup.fillFormEmail("sai123@gmail.com");
         signup.fillFormDob('03 04 1987');
         signup.fillFormWeight('76');
         signup.confirmSignUpButton();
         expect(inputUserName.getText()).toEqual('No numbers/ special character allowed in names');
-        
+       
         
      });
      it('3. Testing FullName with special characters ,\n ', function () {
+      
         var inputUserName = element(by.id('fullNameError'));
-        signup.fillFormFullName("sai345");
+        signup.fillFormFullName("sai$%$##@!");
         signup.fillFormEmail("sai123@gmail.com");
         signup.fillFormDob('03 04 1987');
         signup.fillFormWeight('76');
         signup.confirmSignUpButton();
         expect(inputUserName.getText()).toEqual('No numbers/ special character allowed in names');
-        
+      
+     });
+     it('4. Testing Email with special characters other than @ ,\n ', function () {
+      
+        var inputUserName = element(by.id('emailIncorrect'));
+        signup.fillFormFullName("test five");
+        signup.fillFormEmail("sai123*()(@gmail.com");
+        signup.fillFormDob('03 04 1987');
+        signup.fillFormWeight('76');
+        signup.confirmSignUpButton();
+        expect(inputUserName.getText()).toEqual('Email should be in "****@****.***"');
+      
+     });
+     it('5. Testing Dob format,\n ', function () {
+      
+        var inputUserName = element(by.id('dobError'));
+        signup.fillFormFullName("test six");
+        signup.fillFormEmail("test@gmail.com");
+        signup.fillFormDob('ab/mmc/sass');
+        signup.fillFormWeight('76');
+        signup.confirmSignUpButton();
+        expect(inputUserName.getText()).toEqual('Please give a proper date (MM/DD/YYYY)."');
+      
      });
 
 });
